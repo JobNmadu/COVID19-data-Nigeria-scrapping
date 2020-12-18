d6e37a0104d5e93b33fec9bc1165c8c218ee84dc
Prof. Job N Nmadu <job.nmadu@gmail.com>
2020-12-18 18:29:55 +0100

    Update srapping_from_website.R

--- srapping_from_website.R
+++ srapping_from_website.R
@@ -7,7 +7,7 @@ url <- "https://covid19.ncdc.gov.ng/report/"
 
 COVID19 <- url %>%
   read_html() %>%
-  html_nodes(xpath='//*[@id="custom1"]') %>%
+  html_nodes(xpath='//*[@id="custom1"]') %>%    #get the xpath by right-clicking on the website
   html_table()
 
 COVID19 <- COVID19[[1]]
