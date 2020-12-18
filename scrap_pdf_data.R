987114ff8fce13102d0ef6c6d3fdadb64e1b7aa8
Prof. Job N Nmadu <job.nmadu@gmail.com>
2020-12-18 18:32:51 +0100

    Update scrap_pdf_data.R

--- scrap_pdf_data.R
+++ scrap_pdf_data.R
@@ -1,20 +1,12 @@
 
 library(pdftools)
 library(tidyverse)
-text <- pdf_text("PDF/An update of COVID-19 outbreak in Nigeria_030920_36.pdf") %>%
+text <- pdf_text("PDF/An update of COVID-19 outbreak in Nigeria_030920_36.pdf") %>% #the PDF file is downloaded into a drive and then the data is scrapped
   readr::read_lines()
-text
+text   # displaing the text will give the relevant lines where the data appears which is saved as DATA
 Data <- as.data.frame(text[48:92])
 write_delim(Data, "XLSX/030920.csv", delim = " ")
 
 KK <- data.frame(read_delim("XLSX/020920.csv", delim = " ",
                             skip = 1, skip_empty_rows = TRUE,
                             trim_ws = TRUE, col_names = FALSE))
-
-library(splitstackshape)
-cSplit(KK, c("CONFIRMED CASES          DISCHARGED CASES                  DEATHS                              DAYS SINCE", "TOTAL" ,"LAST", "STATES                                                                                             ACTIVE")
-, sep = " ")
-
-#xlsx::write.xlsx2(DData, file = "DATA2902.xlsx")
-#readr::write_csv2(Data, "DATA3108.csv")
-
